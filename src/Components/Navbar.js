c563168ba06565cb995a4c63422fd39da6d5a70c
0607mathi <mathiyarasu.m0607@gmail.com>
2024-04-05 17:13:49 +0530

    responsive Navbar Completed....:)

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -6,23 +6,24 @@ import Sm_navbar from './Sm_navbar';
 const Navbar = () => {
   return (
     <div>
-         <div className='hidden md:block'>
-        <ul className='Navbar flex justify-center items-center gap-10 my-1'>
-        <li><a href="#"></a><img src={Apple_logo} className='w-12' alt="" /></li>
-        <li><a href="#" className=''>Store</a></li>
-        <li><a href="#">Mac</a></li>
-        <li><a href="#">iPad</a></li>
-        <li><a href="#">iPhone</a></li>
-        <li><a href="#">Watch</a></li>
-        <li><a href="#">Airpods</a></li>
-        <li><a href="#">TV & Home</a></li>
-        <li><a href="#">Entertainment</a></li>
-        <li><a href="#">Accessories</a></li>
-        <li><a href="#">Support</a></li>
-        <li><a href="#" ><AiOutlineSearch className='text-xl'/></a></li>
-        <li><a href="#"><AiOutlineShopping className='text-xl'/></a></li>
-        <li><a href="#"></a></li>
-    </ul></div>  
+         <div className='hidden lg:block bg-white'>
+            <ul className='Navbar flex justify-center items-center'>
+                <li><a href="#"></a><img src={Apple_logo} className='w-12' alt="" /></li>
+                <li><a href="#" className=''>Store</a></li>
+                <li><a href="#">Mac</a></li>
+                <li><a href="#">iPad</a></li>
+                <li><a href="#">iPhone</a></li>
+                <li><a href="#">Watch</a></li>
+                <li><a href="#">Airpods</a></li>
+                <li><a href="#">TV & Home</a></li>
+                <li><a href="#">Entertainment</a></li>
+                <li><a href="#">Accessories</a></li>
+                <li><a href="#">Support</a></li>
+                <li><a href="#" ><AiOutlineSearch className='text-xl'/></a></li>
+                <li><a href="#"><AiOutlineShopping className='text-xl'/></a></li>
+                <li><a href="#"></a></li>
+            </ul>
+         </div>  
          <Sm_navbar />
     </div>
   )
--- src/Components/Sm_navbar.js
+++ src/Components/Sm_navbar.js
@@ -1,15 +1,40 @@
 import React from 'react'
+import { useState,useEffect } from 'react';
+import '../App.css'
+import AOS from 'aos';
+import 'aos/dist/aos.css'; 
 import Apple_logo from '../asserts/images/navbar-apple-logo.png'
 import { AiOutlineSearch,AiOutlineShopping,AiOutlineMenu} from "react-icons/ai";
+import { FaAngleRight } from "react-icons/fa6";
 const Sm_navbar = () => {
+  const[toggleMenu, setToggleMenu]=useState(false);
+  useEffect(() => {
+    AOS.init();
+  }, [])
   return (
-    <div className='flex items-center py-1 mx-1 mr-4'>
-        <a href="#"></a><img src={Apple_logo} className='w-12' alt="" />
-        <ul className='flex gap-7 ml-auto'>
-            <li><a href="#" ><AiOutlineSearch size={22}/></a></li>
-            <li><a href="#"><AiOutlineShopping size={22}/></a></li>
-            <li><button><AiOutlineMenu size={22}/></button></li>
+    <div>
+      <div className='flex items-center py-1 pt-2 mx-1 lg:hidden w-full fixed top-0'>
+      { !toggleMenu &&  <a href="#"><img src={Apple_logo} className='w-14' alt="" /></a> }
+        <ul className='flex gap-7 ml-auto pr-3'>
+        { !toggleMenu &&  <li><a href="#" ><AiOutlineSearch size={20}/></a></li>}
+           { !toggleMenu &&  <li><a href="#"><AiOutlineShopping size={20}/></a></li>}
+            <li><button onClick={()=> setToggleMenu(!toggleMenu)}><AiOutlineMenu size={20}/></button></li>
         </ul>
+      </div>
+      { toggleMenu && <div className="dropdown  w-full bg-white mt-12 block lg:hidden h-screen" data-aos="fade-down" data-aos-easing="linear" data-aos-duration="300">
+        <ul className='mx-12 font-bold mt-1'>
+                <li><a href="#">Store</a><FaAngleRight className='hidden FaAngleRight' /></li>
+                <li><a href="#">Mac</a><FaAngleRight className='hidden FaAngleRight' /></li>
+                <li><a href="#">iPad</a><FaAngleRight className='hidden FaAngleRight' /></li>
+                <li><a href="#">iPhone</a><FaAngleRight className='hidden FaAngleRight' /></li>
+                <li><a href="#">Watch</a><FaAngleRight className='hidden FaAngleRight' /></li>
+                <li><a href="#">Airpods</a><FaAngleRight className='hidden FaAngleRight' /></li>
+                <li><a href="#">TV & Home</a><FaAngleRight className='hidden FaAngleRight' /></li>
+                <li><a href="#">Entertainment</a><FaAngleRight className='hidden FaAngleRight' /></li>
+                <li><a href="#">Accessories</a><FaAngleRight className='hidden FaAngleRight' /></li>
+                <li><a href="#">Support</a><FaAngleRight className='hidden FaAngleRight' /></li>
+        </ul>
+      </div>}
     </div>
   )
 }
